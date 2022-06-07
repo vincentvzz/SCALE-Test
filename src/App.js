6a3917bbff2365fba26efacbe925fb023bbb681c
vzz <vzz@uw.edu>
2022-06-06 23:07:19 -0700

    add basic component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Tag from "./components/tag";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Tag></Tag>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/tag.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Tag() {
+    return (
+        <div>This is a tag</div>
+    )
+}
+
+export default Tag;
